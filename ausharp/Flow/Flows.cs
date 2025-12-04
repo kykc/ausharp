1a2866db688bccad3172ed99f5d0dc6e664a5496
Alexander Kykc <mad.kykc@gmail.com>
2025-12-04 03:23:22 +0200

    Add few operations

--- ausharp/Flow/Flows.cs
+++ ausharp/Flow/Flows.cs
@@ -21,4 +21,35 @@ public static class Flows
     {
         return Val(value.RefVal(), context);
     }
+
+    public static Flow<Value<TSubj>> RefValOr<TSubj>(TSubj? value, string error, IFlowContextHandler? context = null) where TSubj : struct
+    {
+        return value.HasValue ? Val(value.Value.RefVal(), context) : Err<Value<TSubj>>(error, context);
+    }
+
+    public static Flow<TSubj> FirstValOrErr<TSubj>(string? error, params Func<Flow<TSubj>>[] providers) where TSubj : class
+    {
+        string? lastError = null;
+        
+        foreach (var provider in providers)
+        {
+            var result = provider();
+            
+            if (result is { IsVal: true })
+            {
+                return result;
+            }
+            else
+            {
+                lastError = result.UnwrapErr();
+            }
+        }
+
+        return new((error ?? lastError)!);
+    }
+
+    public static Flow<TSubj> FirstValOrLastErr<TSubj>(params Func<Flow<TSubj>>[] providers) where TSubj : class
+    {
+        return FirstValOrErr(null, providers);
+    }
 }
